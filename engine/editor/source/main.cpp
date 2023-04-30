b61b95fb00d290e8241692d1607705478e1f5ae3
hiecxz123 <2277520399@qq.com>
2023-05-01 00:31:49 +0800

    引入GLAD

--- engine/editor/source/main.cpp
+++ engine/editor/source/main.cpp
@@ -1,29 +1,8 @@
 #include<iostream>
-#include<math/math.h>
-#include<math/matrix4.h>
-#include <stdlib.h>
-#include<log/Log.h>
+#include<render/render.cpp>
 using namespace std;
 int main()
 {
-	Core::Math::Mat4x4 mat4x41(
-		1, 1, 1, 1,
-		2, 3, 3, 3,
-		1, 1, 1, 1,
-		2, 2, 2, 2
-	);
-	Core::Math::Mat4x4 mat4x42(
-		1, 1, 1, 1,
-		2, 2, 2, 2,
-		3, 3, 3, 3,
-		4, 4, 4, 4
-	);
-	Core::Math::Mat4x4 temp;
-	temp = mat4x41 * mat4x42;
-
-
-	temp.PrintMat4x4();
-
-	system("pause");
+	RenderLoop();
 
 }
