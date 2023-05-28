1a8e705f98ae70235a700aa42572be3e895833c6
hiecxz123 <2277520399@qq.com>
2023-05-28 17:24:10 +0800

    添加着色器类

--- engine/core/render/shader/shader.h
+++ engine/core/render/shader/shader.h
@@ -7,5 +7,13 @@ public:
 	std::string fragShaderPath;
 	std::string vertexCode;
 	std::string fragCode;
+	Shader(const char* vertexPath,const char* fragmentPath);
+	~Shader();
+	void Use();
+	void SetBool(const std::string& name, bool value); 
+	void SetInt(const std::string& name, int value);
+	void SetFloat(const std::string& name, float value);
+private:
+	void CheckComplieErrors(unsigned int shader, std::string type);
 
 };
